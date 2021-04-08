b337fadca85107d4730c0926c99ca3e54fe8c5c0
Jake Desktop <jm9357481@gmail.com>
2021-04-07 19:09:00 -0700

    [JS] -adds notes for dark theme button functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-// js content 
-alert('hello');
+// js content --- button functionality
+
+//define dark theme function
+//  let button = false
+//  if (dark theme button is toggled)
+//      if button === false
+//          button = true
+//      else
+//          button = false
+//  if (dark theme button is true):
+//      assign css body a dark background-color
+//      assign css body a light color
+//      assign dark theme button with new text 'Light Theme'
+//      ^^^change dark theme button color as well^^^
+//  else if (dark theme button is false):
+//      assign css body original background-color
+//      assign css body original color
+//      assign dark theme button with original text 'Dark Theme'
+//      ^^^change dark theme button color to original^^^
+
