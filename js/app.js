1310f24a1cefaf93c28af17acd518a8450e77be8
Jake Desktop <jm9357481@gmail.com>
2021-04-07 19:35:19 -0700

    [JS] -adds notes for cancel button functionality

--- js/app.js
+++ js/app.js
@@ -7,14 +7,25 @@
 //          button = true
 //      else
 //          button = false
-//  if (dark theme button is true):
+//  if (dark theme button is true)
 //      assign css body a dark background-color
 //      assign css body a light color
 //      assign dark theme button with new text 'Light Theme'
 //      ^^^change dark theme button color as well^^^
-//  else if (dark theme button is false):
+//  else if (dark theme button is false)
 //      assign css body original background-color
 //      assign css body original color
 //      assign dark theme button with original text 'Dark Theme'
 //      ^^^change dark theme button color to original^^^
+
+//define cancel function
+//  var cancel_button = false
+//  if (cancel_button is toggled)
+//      if cancel_button === false
+//          cancel_button = true
+//      else
+//          cancel_button = false
+//  if (cancel_button is true)
+//      assign css note_taker with display: none
+//      assign css bottom_button with display: none
 
