f919fe340138e47bac7b61ac46aa8d614a709112
Jake Desktop <jm9357481@gmail.com>
2021-04-13 13:11:35 -0700

    [JS,css] -removes notes at bottom of JS script -adds function to select specific sidebar notes and view notes in textbox -gives the sidebar heading center text alignment in css

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const dark_theme_btn = document.querySelector("#dark");
 const new_note_btn = document.querySelector("#new_note");
 const save_btn = document.querySelector("#save");
 const cancel_btn = document.querySelector("#cancel");
-const sidebar = document.querySelector("#save_notes_here")
 let hidden_elements = null;
 const notesArray = [{title:"note one", body:"this is my first note"},
                     {title:"note two", body:"this is my second note"}];
@@ -56,56 +55,18 @@ function save() {
     }
 }
 
-function note_clicked() {
-
-}
+document.getElementById("save_notes_here").addEventListener("click", function(e) {
+    if (e.target && e.target.nodeName == "LI") {
+        alert(`Note: ${e.target.textContent} has been selected!`);
+    }
+    for (let item of notesArray) {
+        if (item.title.includes(e.target.textContent) === true) {
+            document.getElementById("take_note").value = item.body;
+        }
+    }
+});
 
 dark_theme_btn.addEventListener('click', theme);
 cancel_btn.addEventListener('click', cancel);
 new_note_btn.addEventListener('click', new_note);
 save_btn.addEventListener('click', save);
-
-//define dark theme function
-//  let button = false
-//  if (dark theme button is toggled)
-//      if button === false
-//          button = true
-//      else
-//          button = false
-//  if (dark theme button is true)
-//      assign css body a dark background-color
-//      assign css body a light color
-//      assign dark theme button with new text 'Light Theme'
-//      ^^^change dark theme button color as well^^^
-//  else if (dark theme button is false)
-//      assign css body original background-color
-//      assign css body original color
-//      assign dark theme button with original text 'Dark Theme'
-//      ^^^change dark theme button color to original^^^
-
-//define cancel function
-//  var cancel_button = false
-//  if (cancel_button is toggled)
-//      if cancel_button === false
-//          cancel_button = true
-//      else
-//          cancel_button = false
-//  if (cancel_button is true)
-//      assign css note_taker with visibility:hidden
-//      assign css bottom_button with visibility:hidden
-
-//define new note function
-//  let clear_note = none
-//  if (new note button is toggled)
-//      if clear_note === none
-//          clear_note = false
-//      else if clear_note === false
-//          clear_note = true
-//      else
-//          clear_note = none
-//  if (cancel_button === true) and (clear_note === false)
-//      assign css note_taker with visibility:visible
-//      assign css bottom_button with visibility:visible
-//      assign cancel_button with false
-//  else if (cancel_button === false) and (clear_note === true)
-//      clear textarea
